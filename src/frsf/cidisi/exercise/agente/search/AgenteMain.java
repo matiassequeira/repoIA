2274dbea9bd6b5f1ca5c4ced5bc9f15eb927db74
matiassequeira <matiassequeirq@gmail.com>
2016-05-20 00:17:32 -0300

    subo agente main que habia borrado

--- src/frsf/cidisi/exercise/agente/search/AgenteMain.java
+++ src/frsf/cidisi/exercise/agente/search/AgenteMain.java
@@ -1,18 +1,69 @@
 package frsf.cidisi.exercise.agente.search;
 
+import frsf.cidisi.exercise.agente.search.Agente;
+import frsf.cidisi.exercise.agente.search.EstadoAgente;
+import frsf.cidisi.exercise.agente.search.Facultad;
 import frsf.cidisi.faia.exceptions.PrologConnectorException;
 import frsf.cidisi.faia.simulator.SearchBasedAgentSimulator;
+import interfaz.Frame;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class AgenteMain {
-
-    public static void main(String[] args) throws PrologConnectorException {
+    private Frame frame;
+    public AgenteMain() throws IOException{
         Agente agent = new Agente();
 
         Facultad environment = new Facultad();
+        EstadoAgente estadoAgente= (EstadoAgente) agent.getAgentState();
+        frame= new Frame(estadoAgente.getMapa(),this); //Se envia agenteMain para ejecutar start()
+        
+        // 1. create the pipes
+        PipedInputStream inPipe = new PipedInputStream();
+        PipedInputStream outPipe = new PipedInputStream();
+
+        // 2. set the System.in and System.out streams
+        System.setIn(inPipe);
+        System.setOut(new PrintStream(new PipedOutputStream(outPipe), true));
+
+
+        PrintWriter inWriter = new PrintWriter(new PipedOutputStream(inPipe), true);
+
+        
+        
+        frame.addConsola(outPipe, inWriter);
+        
+
+        
+
+        
+        //Se setea la ventana para graficar el movimiento del agente en tiempo real
+        environment.getEnvironmentState().setFrame(frame);
+
+        // 5. get some input (from JTextArea)
+        Scanner s = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) throws PrologConnectorException, IOException {
+        
+        new AgenteMain();
+         
+        
+    }
+    
+    public void simulatorStart(){
+        Agente agent = new Agente();
+
+        Facultad environment = new Facultad();
+        
+        environment.getEnvironmentState().setFrame(frame);
 
         SearchBasedAgentSimulator simulator =
                 new SearchBasedAgentSimulator(environment, agent);
-        
         simulator.start();
     }
 
